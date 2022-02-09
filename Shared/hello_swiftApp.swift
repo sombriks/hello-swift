93726573df16f51404d6f8e54272e7a3036a282c
Leonardo Silveira <sombriks@gmail.com>
2022-02-09 16:46:22 -0300

    Initial Commit

---
+++ Shared/hello_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  hello_swiftApp.swift
+//  Shared
+//
+//  Created by Leonardo Silveira on 09/02/22.
+//
+
+import SwiftUI
+
+@main
+struct hello_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
